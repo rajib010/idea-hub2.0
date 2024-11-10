d61a0bd573eaf2d3f0a4d20e4c065114bba151fa
raJiB <pokhrelrajib016@gmail.com>
2024-11-10 18:59:07 +0545

    added github login system

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { auth, signOut, signIn } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className='px-5 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items-center text-black'>
+        <Link href="/">
+          <Image src="/idea-hub-logo.png" alt="logo" width={100} height={5} />
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form action={async () => {
+                "use server";
+                await signOut({ redirectTo :"/"});
+              }}>
+              <button type='submit'>
+                Log Out
+              </button>
+            </form>
+
+          <Link href={`/user/${session?.user?.id}`}>
+            <span>{session?.user?.name}</span>
+          </Link>
+        </>
+        ) : (
+        <form action={async () => {
+          "use server";
+          await signIn('github')
+        }}>
+          <button type='submit'>
+            Login
+          </button>
+        </form>
+          )}
+      </div>
+    </nav>
+    </header >
+  );
+};
+
+export default Navbar;
