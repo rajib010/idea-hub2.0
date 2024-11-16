c3e37ce9a9f931420b4d323a91d7f231872dcdaa
raJiB <pokhrelrajib016@gmail.com>
2024-11-16 10:29:10 +0545

    added editor picks section

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -42,9 +42,6 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
        bio
 }`);
 
-export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`
-   `)
-
 export const AUTHOR_BY_GITHUBID_QUERY = defineQuery(`
    *[_type=="author" && id==$id][0]{
       _id,
@@ -70,3 +67,27 @@ export const AUTHOR_BY_GITHUBID_QUERY = defineQuery(`
    category,
    image,
  }`);
+
+ export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}`);
